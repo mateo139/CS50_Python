5b361e22ad86dd421d70a322180e753e7350f8b5
mateo139 <103432222+mateo139@users.noreply.github.com>
2022-11-08 06:02:43 +0100

    Libraries excercises

---
+++ Libraries/generate_random.py
@@ -0,0 +1,14 @@
+import random
+
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
+
+# import random
+# number = random.randint(1,100)
+# print (number)
+
+# from random import choice
+# coin = choice(["heads", "tails"])  # inside is list[]
+# print(coin)
