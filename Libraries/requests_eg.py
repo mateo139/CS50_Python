7cdc4eae18293e7da6b34e73ca4c88ce0f4b3798
mateo139 <103432222+mateo139@users.noreply.github.com>
2022-11-12 15:41:59 +0100

    impor_from_own_lib

--- Libraries/requests_eg.py
+++ Libraries/requests_eg.py
@@ -1,6 +1,7 @@
 # APIs
 # pypi.orf/project/requests
 # JSON
+#sys.exit()
 
 import json
 import requests
@@ -10,7 +11,7 @@ if len(sys.argv) != 2:
     sys.exit()
 
 response = requests.get(
-    "https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1]
+    "https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1]
 )
 #print(json.dumps(response.json(), indent=2))
 o = response.json()
