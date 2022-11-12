a5e7a363cb2191bded9f149e7c9c3286b1f3c579
mateo139 <103432222+mateo139@users.noreply.github.com>
2022-11-12 22:35:04 +0100

    styles & libraries exc.

--- Libraries/say.py
+++ Libraries/say.py
@@ -1,5 +1,5 @@
 import sys
-from sayings import hello
+from sayings import goodbye
 
 if len(sys.argv) == 2:
-    hello(sys.argv[1])
+    goodbye(sys.argv[1])
---
+++ Styles/styles.py
@@ -0,0 +1,18 @@
+#   PEP 8
+#   peps.python.org/pep-0008/
+#   "READABILITY COUNTS"
+
+# Indentations !!!
+# Tabs or spaces ? Tabs => spaces
+# Max Line Length
+# Blank Lines
+# Imports 
+
+#   pylint
+#   pip install pylint
+#   pycodestyle => pycodestyle.pycqa.org
+
+##   black !!!
+##   pip install black
+##   black.readthedocs.io
+##   black name_of_file.py
