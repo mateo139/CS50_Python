4f0b262709673d86e13ff7c1271f027d9c452e3a
mateo139 <103432222+mateo139@users.noreply.github.com>
2022-11-14 06:33:02 +0100

    inflect library

---
+++ Libraries/inflect_libr_ver0.py
@@ -0,0 +1,13 @@
+import inflect
+
+p = inflect.engine()
+
+names_list = []
+
+try:
+    while True:
+        names_list.append(input("Name: "))
+
+except (EOFError):
+    print("\nAdieu, adieu, to ", p.join(names_list, final_sep=""), sep="")
+
