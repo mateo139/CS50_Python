c926d3e91100a7ff808f44c551489117b82a165f
mateo139 <103432222+mateo139@users.noreply.github.com>
2022-12-17 14:46:02 +0100

    old commit

--- FileIO/namesIO.py
+++ FileIO/namesIO.py
@@ -14,3 +14,6 @@ with open("names.txt") as file:
     for line in file:
         names.append(line.rstrip())
         # 22 min lecture
+
+for name in sorted(names, reverse = True):
+    print(f"hello, {name}")
