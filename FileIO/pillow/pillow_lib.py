8daa1667632e904a483ed2456a5b9d2ae745f467
mateo139 <103432222+mateo139@users.noreply.github.com>
2023-01-06 15:24:56 +0100

    Pillow_library ex

--- FileIO/pillow/pillow_lib.py
+++ FileIO/pillow/pillow_lib.py
@@ -1,10 +1,22 @@
-from PIL import Image
+from PIL import (
+    Image,
+    ImageOps,
+)
 import sys
 
+
 try:
+    shirt = Image.open("shirt.png")
+    # print(shirt.format, shirt.size, shirt.mode)
+    # shirt.show()
+    shirt = ImageOps.fit(
+        method=Image.Resampling.BICUBIC, size=(1200, 1600), image=shirt
+    )
+
     im = Image.open("before1.jpg")
-    print(im.format, im.size, im.mode)
+    # print(im.size)
+    im.paste(shirt, mask=shirt)
     im.show()
+
 except OSError:
     sys.exit("File can not be opened")
-
