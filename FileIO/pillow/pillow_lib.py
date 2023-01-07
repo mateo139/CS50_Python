790c7aebafaa7753452b4494113149bce5f53652
mateo139 <103432222+mateo139@users.noreply.github.com>
2023-01-07 15:54:24 +0100

    Code change

--- FileIO/pillow/pillow_lib.py
+++ FileIO/pillow/pillow_lib.py
@@ -9,7 +9,7 @@ try:
     # print(shirt.format, shirt.size, shirt.mode)
     # shirt.show()
     shirt = ImageOps.fit(
-        method=Image.Resampling.BICUBIC, size=(1200, 1600), image=shirt
+        method=Image.Resampling.BICUBIC, size=(1200, 1200), image=shirt
     )
 
     im = Image.open("before1.jpg")
