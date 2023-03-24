9000fd7d0b014d686e9fda0160b70b1b59373b42
Mateo <mateo.laska@gmail.com>
2023-03-24 21:31:07 +0100

    OOP tutorials

--- OOP/oop_tutorial3.py
+++ OOP/oop_tutorial3.py
@@ -1,8 +1,25 @@
 class Dog:
 
-    #class attribute 
-    species = "mammal" 
+    # Use class attributes to define properties that should have the same value for every class instance.
+    # Use instance attributes for properties that vary from one instance to another.
 
-    def __init__(self, name, age):
+    # class attribute
+    species = "mammal"
+
+    def __init__(self, name, age):  #  instance attributes are between ()
         self.name = name
         self.age = age
+
+    # instance method
+    def __str__ (self):
+        return f"{self.name} is {self.age} years old"
+    
+    # another instance method
+    def speak (self, sound):
+        return f"{self.name} says {sound}"
+
+
+dog1 = Dog("Markus", "3")
+dog2 = Dog("Henry", "4")
+print (dog1.__str__())
+print (dog1.speak("woof"))
---
+++ OOP/oop_tutorial3_class.py
@@ -0,0 +1,14 @@
+class Car:
+    def __init__(self, color, mileage):
+        self.color = color
+        self.mileage = mileage
+
+    def __str__(self):
+        return f"The {self.color} car has {self.mileage} miles"
+
+
+car1 = Car("blue", "20 000")
+car2 = Car("red", "30 000")
+
+print (car1.__str__())
+print (car2.__str__())
